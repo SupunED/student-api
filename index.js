1aab03f3ce3bd65ae7002ba95f8810491509ca3d
supun <supunpramudithajayawardhana@gmail.com>
2025-07-24 13:47:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+let students = [];
+
+// POST - Add new Student
+app.post('/students', (req, res) => {
+    const student = req.body;
+    students.push(student);
+    res.status(201).json({message: 'Student added', data: student});
+});
+
+// GET - Get all Students
+app.get('/students', (req, res) =>{
+    res.json(students);
+})
+
+// PUT - Update a Student's data
+app.put('/students/:id', (req, res) => {
+    const student_id = req.params.id;
+    const updated_data = req.body;
+
+    let student = students.find(s => s.id == student_id);
+
+    if (student) {
+        Object.assign(student, updated_data);
+        res.json({message: 'Student Updated', data: student});
+    }
+    else {
+        res.status(404).json({message: 'Student not found'});
+    }
+});
+
+// DELETE - Delete a Student by Id
+app.delete('/students/:id', (req, res) => {
+    const student_id = req.params.id;
+    const index = students.findIndex(s => s.id == student_id);
+
+    if (index !== -1) {
+        const deleted = students.splice(index, 1);
+        res.json({message: 'Student deleted', data: deleted[0]});
+    }
+    else {
+        res.status(404).json({message: 'Student not found'})
+    }
+});
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
+});
